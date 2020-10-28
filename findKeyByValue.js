9d9a98c0eeeeecca93ec5ede33cec2a8b4b19a55
Julia Di Monte <dimontejulia@gmail.com>
2020-10-28 19:17:39 +0000

    findKeyByValue added

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (myObj, lookupVal) {
+  // generates a list of keys found myObj
+  const listOfKeys = Object.keys(myObj);
+  //loops through all of the keys and compares them to myObj
+  for (key of listOfKeys) {
+    if (myObj[key] === lookupVal) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); //pass
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); //fail

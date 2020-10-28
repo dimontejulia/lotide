52997c46622fe7dbc8a6ae19203b3ffcec9aea62
Julia Di Monte <dimontejulia@gmail.com>
2020-10-28 18:00:32 +0000

    countLetters.js using the assertEqual function

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const letterCount = {};
+
+  for (letter of sentence) {
+    if (letter === " ") {
+    } else if (letterCount[letter]) {
+      letterCount[letter]++;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+};
+
+const countLighthouse = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};
+console.log(countLetters("lighthouse in the house"));
+console.log(countLighthouse);
+assertEqual(countLetters("lighthouse in the house"), countLighthouse);

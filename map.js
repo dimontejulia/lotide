f26953615608e9290db020688f1f9203e70e54db
Julia Di Monte <dimontejulia@gmail.com>
2020-10-29 18:25:13 +0000

    map.js added

---
+++ map.js
@@ -0,0 +1,42 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const empty = [];
+
+const eqArrays = function (array1, array2) {
+  if (
+    array1 === undefined ||
+    array2 === undefined ||
+    array1.length !== array2.length
+  ) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+const results2 = map(empty, (word) => word[0]);
+console.log(results2);
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]); // => true
+assertArraysEqual(results1, ["bad", "c", "t", "m", "t"]); // => false
+assertArraysEqual(results2, []); // => true

12919279b5c7d1728e26fb5385b19ece236b4bae
Julia Di Monte <dimontejulia@gmail.com>
2020-11-02 17:54:42 +0000

    refractoring original and test files

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const eqArrays = require("../eqArrays");
 
 //TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 eqArrays([1, 2, 3], [3, 2, 1], true); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"], true); // => true
